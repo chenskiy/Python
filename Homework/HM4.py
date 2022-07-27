b9e81a6aa0890c1503dcfc855b25f25f2a969724
chenskiy_vladislav <chenskiy.vlad@mail.ru>
2022-07-27 23:58:20 +0300

    HM4

--- Homework/HM4.py
+++ Homework/HM4.py
@@ -4,10 +4,10 @@
 #     Пример:
 #     - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
-import math
+from math import pi
 
 def accuracy(d):
-    p = math.pi 
+    p = pi 
     count = 0 
     for i in d:
         count+=1
@@ -19,3 +19,125 @@ def accuracy(d):
 
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
+def prime_factors(n):
+    k = []
+    for i in range(1, n+1):
+        if n % i == 0: 
+            k.append(i)
+    return k
+
+# print (prime_factors(int(input('Введите число: '))))
+
+# 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+def non_repeating(n):
+    ls = []
+    for i in n:
+        if i not in ls:
+            ls.append(i)
+    return(ls)
+
+# numbers = [1, 2, 3, 5, 1, 5, 3, 10]
+# print(non_repeating(numbers))
+
+
+# 4.  Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+#     Пример:
+#     - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import random
+print('\n-----4 задача-----\n')
+def rand(k): 
+    r = []
+    for i in range(k + 1):
+        i = random.randint(0,101)
+        r.append(i)
+    return r
+
+def assembling(r):  #заполняем строку многочленами
+    st = ''
+    for i in range(len(r)-1, -1, -1):
+        if r[i] > 1:
+            if i == 0:
+                st += str(r[i]) + ' + '
+            elif i == 1:
+                st += str(r[i]) + 'x' + ' + '
+            else:
+                st += str(r[i]) + 'x^' + str(i) + ' + '
+        if r[i] == 1:
+            if i == 0:
+                st += ' + '
+            elif i == 1:
+                st += 'x' + ' + '
+            else:
+                st += 'x^' + str(i) + ' + '          
+    st = st[:-3]
+    st += ' = 0'
+    return st
+
+k = int(input("Введите натуральную степень: ")) 
+r = rand(k) 
+print(f'{r} - Сформированый случайным образом список коэффициентов')
+a = assembling(r)
+print(f'{a} - записаный в файл многочлен в степени {k}')
+data = 'Task4.txt'              #Запись пойдет также в 5 задачу
+with open(data, 'w') as f_1:
+    f_1.write(a)
+
+
+# 5. Даны два файла в каждом из которых находится запись многочлена. Сформировать файл содержащий сумму многочленов.
+
+print('\n-----5 задача-----\n')
+
+
+def pure_polynomials(a):       # Отделение коэффициентов 
+    a = a[:-3]
+    a = a.replace(' ', '').replace('^', '').split('+')
+    c = ''
+    b = []
+    for i in range(len(a)):
+        c = ''
+        for j in a[i]:
+            if j != 'x':
+                c += str(j)
+            else: break
+        c = int(c)
+        b.append(c)
+    return b
+
+def summ(f1, f2):   # суммируем коэф. многочлена
+    sum=[]
+    for i in range(len(f1)):
+        sum.append(f1[i] + f2[i])
+    return sum
+
+r = rand(k) 
+print(f'{r} - Сформированый случайным образом список(2) коэффициентов')
+a = assembling(r)
+print(f'{a} - записаный в файл(2) многочлен в степени {k}')
+
+path = 'Task5.txt'              
+with open(path, 'w') as f_2:
+    f_2.write(a)
+
+with open(data, 'r') as f_1:
+    f_1 = f_1.readline()
+
+with open(path, 'r') as f_2:
+    f_2 = f_2.readline()
+
+ppf_1 = pure_polynomials(f_1)
+ppf_2 = pure_polynomials(f_2)
+
+print(f'{ppf_1} коэффициенты многочлена из 1-го файла')
+print(f'{ppf_2} коэффициенты многочлена из 2-го файла')
+
+s = summ(ppf_1, ppf_2)
+print(f'{s} сумма многочленов')
+
+assemb = assembling(s)
+print(f'{assemb} Записана в файл(3) сумма коэф.')
+
+dpat = 'Task5(end).txt'
+with open(dpat, 'w') as f_3:
+    f_3.write(assemb)
--- Tasks/Sem5-6.py
+++ Tasks/Sem5-6.py
@@ -1,8 +1,8 @@
 # 32. Составить список простых множителей натурального числа N
 
-# number = int(input("введите число: "))
-# k = [i for i in range(1, number+1) if(number % i == 0)]
-# print(k)
+    # number = int(input("введите число: "))
+    # k = [i for i in range(1, number+1) if(number % i == 0)]
+    # print(k)
 
 # 33. Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
 #     Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
@@ -14,25 +14,57 @@
 # 34. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 #     *Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-def random_coef(k):
-    from random import randint
-    random_coef_list = [randint(0,101) for i in range(k+1)]
-    return random_coef_list
+import random
 
+def rand(k): 
+    r = []
+    for i in range(k + 1):
+        i = random.randint(0,101)
+        r.append(i)
+    return r
 
-def polinom(list_arg):
-    k = len(list_arg) - 1
-    result_list = [str(list_arg[i])+'*x**'+str(k-i) for i in range(k-1) if list_arg[i] != 0]
-    if list_arg[k-1] != 0:
-        result_list.append(str(list_arg[k-1])+'*x')
-    if list_arg[k] != 0:
-        result_list.append(str(list_arg[k])) 
-    result = ' + '.join(result_list) + ' = 0'
-    return result
+def assembling(r):
+    st = ''
+    for i in range(len(r)-1, -1, -1):
+        if r[i] > 1:
+            if i == 0:
+                st += str(r[i]) + ' + '
+            elif i == 1:
+                st += str(r[i]) + 'x' + ' + '
+            else:
+                st += str(r[i]) + 'x^' + str(i) + ' + '
+        elif r[i] == 1:
+            if i == 0:
+                st += ' + '
+            elif i == 1:
+                st += 'x' + ' + '
+            else:
+                st += 'x^' + str(i) + ' + '          
+    st = st[:-3]
+    st += ' = 0'
+    return st
 
+k = int(input("Введите натуральную степень: ")) 
+r = rand(k) 
+print(r)
+a = assembling(r)
+print(a)
 
-k = int(input("Введите показатель степени многочлена: "))
-with open('file.txt', 'w') as data:
-    data.write(polinom(random_coef(k)))
 
 # 35. Даны два файла в каждом из которых находится запись многочлена. Сформировать файл содержащий сумму многочленов.
+
+a = a[:-3]
+a = a.replace(' ', '').replace('^', '').split('+')
+print(a)
+c = ''
+b = []
+for i in range(len(a)):
+    c = ''
+    for j in a[i]:
+        if j != 'x':
+            c += str(j)
+        else: break
+    c = int(c)
+    b.append(c)
+print(a)
+print(b)
